d4200708b751db92bd5cf2abce9411ee1130caef
DESTERIO-RASMENTO <desochudi@gmail.com>
2021-04-22 21:15:04 +0300

    initial commit

---
+++ src/assets/images/image3.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReacrRoundedImage from 'react-rounded-image'
+import image from './img3.jpg'
+function ImageThree() {
+  return (
+    <div>
+      <ReacrRoundedImage image={image}
+      imageWidth="130"
+      imageHeight="130"/>
+    </div>
+  )
+}
+
+export default ImageThree
